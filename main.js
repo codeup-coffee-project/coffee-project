30ad3ed93929eb1bd7098c45a82a9cb95141f076
MichaelWaymire <mike.waymire2@gmail.com>
2018-08-22 19:50:45 -0500

    worked on coffee exercise, will need to pull source and look over the difference

--- main.js
+++ main.js
@@ -1,37 +1,63 @@
-"use strict"
+"use strict";
 
+let tbody = document.querySelector('#coffees');
+let submitButton = document.querySelector('#submit');
+let roastSelection = document.querySelector('#roast-selection');
+
+
+
+console.log(tbody);
+// function renderCoffee(coffee) {
+//     let html = '<div class="coffee">';
+//     html += '<div>' + coffee.id + '</div>';
+//     html += '<div>' + coffee.name + '</div>';
+//     html += '<div>' + coffee.roast + '</div>';
+//     html += '</div>';
+//
+//     return html;
+// }
+//
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
-
+    let html = '<div class = "coffee">';
+    console.log("in rendercoffee")
+    console.log(coffee);
+    html += '<div class= "name">' + coffee.name + '<span class= "roasts">' + coffee.roast + '</span></div>';
+    html += '</div>';
     return html;
 }
 
 function renderCoffees(coffees) {
-    var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
-        html += renderCoffee(coffees[i]);
+    let html = '';
+    for (let i = 0; i <= coffees.length -1; i++) {
+        console.log("In rendercoffees: ")
+        console.log(coffees[i]);
+	    html += renderCoffee(coffees[i]);
     }
     return html;
+
 }
+
+
 
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
+    let selectedRoast = roastSelection.value;
+    let filteredCoffees = [];
+    console.log(selectedRoast);
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
         }
     });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
+
+
+tbody.innerHTML= renderCoffees(filteredCoffees);
 }
 
+
+
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
+let coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
     {id: 2, name: 'Half City', roast: 'light'},
     {id: 3, name: 'Cinnamon', roast: 'light'},
@@ -48,15 +74,37 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
-function makeTopDropListChange() {
-	var topDropList = document.getElementById("top-pick-that-coffee")[0].value;
-}
+// let listItems = document.getElementsByTagName('h3');
+// function listCoffeeTypes(coffees) {
+//
+// 	for (let i = 0; i < listItems.length; i++) {
+// 		console.log(listItems[i]);
+// 		// input = document.getElementsByTagName('h3');
+// 		// console.log(input);
+// 		listItems += listCoffeeTypes(coffees[i]);
+// 	}
+// 	return listItems;
+
+// }
+// console.log(listCoffeeTypes());
 
 
-var tbody = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
 
-tbody.innerHTML = renderCoffees(coffees);
+
+
+//
+// 	let renderTheCoffee = document.getElementsByTagName("h3");
+// 		for (let i = 0; i < renderTheCoffee().length; i++) {
+//
+//
+// 	}
+// }
+// coffeeListings();
+
+
+
+
+
+tbody.innerHTML = renderCoffee(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
